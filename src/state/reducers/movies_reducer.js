d6221700c696f51bede42978b7ef7af14b18d37e
Yasser Batas <yasser.batas@gmail.com>
2019-03-02 03:35:43 -0600

    Delete some comments

--- src/state/reducers/movies_reducer.js
+++ src/state/reducers/movies_reducer.js
@@ -11,10 +11,6 @@ export const switchcase = cases => defaultCase => key =>
 export const switchcaseF = cases => defaultCase => key =>
   executeIfFunction(switchcase(cases)(defaultCase)(key));
 
-// CLEAR_BOARD: () => ({ gameState: action.payload }),
-// SET_NEW_GAME_STATE: () => ({ ...state, gameState: action.payload }),
-// UPDATE_FIGURE: () => ({ currentFigure: action.payload })
-
 export default (state = INITIAL_STATE, action) =>
   switchcaseF({
     SET_MOVIES_LIST: () => ({ ...state, moviesList: action.payload }),
