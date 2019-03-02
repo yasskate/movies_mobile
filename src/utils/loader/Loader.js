2cc1b02f88aaefeb0051b8684c132f1a66a62b05
Yasser Batas <yasser.batas@gmail.com>
2019-03-02 03:45:55 -0600

    delete unnecessary comments

--- src/utils/loader/Loader.js
+++ src/utils/loader/Loader.js
@@ -2,11 +2,9 @@ import React from 'react';
 import { View, ActivityIndicator } from 'react-native';
 
 export const renderLoader = () => {
-  // if (isLoading) {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center' }}>
-        <ActivityIndicator size="large" color="#0000ff" />
-      </View>
-    );
-  // }
+  return (
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+      <ActivityIndicator size="large" color="#0000ff" />
+    </View>
+  );
 };
