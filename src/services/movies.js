0451d698710a0278b55656c43e41d8c6686d3d46
Yasser Batas <yasser.batas@gmail.com>
2019-03-04 18:39:46 -0600

    update api url from localhost to heroku app url

--- src/services/movies.js
+++ src/services/movies.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
+const API_URL = 'https://movies-api-koa.herokuapp.com';
+
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 axios.defaults.headers.common['Accept'] = 'application/json';
 
 export default {
   getMoviesList: async () => {
-    return axios.get('http://localhost:3000/api/v1/movies');
+    return axios.get(`${API_URL}/api/v1/movies`);
   }
-}
+};
