21e2a255fea07f72d85a2f05d55059d176f0d128
Yasser Batas <yasser.batas@gmail.com>
2019-07-01 11:09:45 -0500

    add mock file

---
+++ src/utils/mocks/movies_mocks.js
@@ -0,0 +1,29 @@
+export const moviesListMockResponse = [
+  {
+    id: 1,
+    name: 'The Land Before Time',
+    genre: 'Fantasy',
+    rating: 7,
+    explicit: false,
+    cover_image:
+      'https://images-na.ssl-images-amazon.com/images/I/51DRW0NT8ML._SY445_.jpg'
+  },
+  {
+    id: 2,
+    name: 'Jurassic Park',
+    genre: 'Science Fiction',
+    rating: 9,
+    explicit: true,
+    cover_image:
+      'https://d32qys9a6wm9no.cloudfront.net/images/movies/poster/b1/b151ce4935a3c2807e1dd9963eda16d8_500x735.jpg'
+  },
+  {
+    id: 3,
+    name: 'Ice Age: Dawn of the Dinosaurs',
+    genre: 'Action/Romance',
+    rating: 5,
+    explicit: false,
+    cover_image:
+      'https://images-na.ssl-images-amazon.com/images/I/61oSEbYf%2BTL.jpg'
+  }
+];
