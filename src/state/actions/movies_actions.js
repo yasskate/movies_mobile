13257334c4b1ba5cf2fafefeefa592e57f63dd73
Yasser Batas <yasser.batas@gmail.com>
2019-07-01 11:07:10 -0500

    add action

--- src/state/actions/movies_actions.js
+++ src/state/actions/movies_actions.js
@@ -5,19 +5,19 @@ export const getMoviesList = () => {
   return async dispatch => {
     try {
       const response = await Movies.getMoviesList();
-      dispatch(setMoviesList(response.data.data));
+      return dispatch(setMoviesList(response.data.data));
     } catch (error) {
       console.log('error', error);
     }
   };
 };
 
-export const setSelectedMovie = selectedMovie => ({
-  type: SET_SELECTED_MOVIE,
-  payload: selectedMovie
-});
-
 export const setMoviesList = moviesList => ({
   type: SET_MOVIES_LIST,
   payload: moviesList
+});
+
+export const setSelectedMovie = selectedMovie => ({
+  type: SET_SELECTED_MOVIE,
+  payload: selectedMovie
 });
